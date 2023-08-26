0bbd03560420e6516e7c69bdf31f8deac06022f2
CodingXamuyee <127799815+TheXamuyee@users.noreply.github.com>
2023-08-26 12:01:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+let inputval = document.querySelector("#text");
+let btn = document.querySelector(".btn");
+let res = document.querySelector(".res");
+
+function checker() {
+  let textvalue = inputval.value;
+  let trimmedval = textvalue.replace(/[\W_]/g, "").toLowerCase();
+  let reversedval = trimmedval.split("").reverse().join("");
+  if (trimmedval === reversedval) {
+    res.classList.add("show");
+    res.style.color = "green";
+    res.textContent = textvalue + " si es una palabra palindrome";
+  } else {
+    res.classList.add("show");
+    res.style.color = "red";
+    res.textContent = textvalue + " no es una palabra palindrome";
+  }
+  console.log(trimmedval);
+  console.log(reversedval);
+}
+btn.addEventListener("click", () => {
+  if (inputval.value !== "") {
+    checker();
+  }
+});
+inputval.addEventListener('input',(e)=>{
+    if(e.target.value===''){
+    res.classList.remove('show')
+    }
+})
+inputval.addEventListener('keyup',(e)=>{
+ if(e.keyCode===13 && e.target.value!==''){
+    checker()
+ }
+})

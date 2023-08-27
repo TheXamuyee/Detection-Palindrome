7fa24afd770b3c89d9c7ed40bee732db61f4a3a0
CodingXamuyee <127799815+TheXamuyee@users.noreply.github.com>
2023-08-27 14:48:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ let res = document.querySelector(".res");
 
 function checker() {
   let textvalue = inputval.value;
-  let trimmedval = textvalue.replace(/[\W_]/g, "").toLowerCase();
+  let trimmedval = textvalue.replace(/[\W_]/, "g").toLowerCase();
   let reversedval = trimmedval.split("").reverse().join("");
   if (trimmedval === reversedval) {
     res.classList.add("show");
     res.style.color = "green";
     res.textContent = textvalue + " si es una palabra palindrome";
+    setTimeout(() => {
+      res.classList.remove("show");
+      inputval.value = '';
+    }, 2000);
   } else {
     res.classList.add("show");
     res.style.color = "red";
     res.textContent = textvalue + " no es una palabra palindrome";
+    setTimeout(() => {
+      res.classList.remove('show');
+      inputval.value = '';
+    }, 2000);
   }
   console.log(trimmedval);
   console.log(reversedval);
